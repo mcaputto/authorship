3ffa2792e3629881f17d0d463016ba602109e780
mcaputto <mcaputto@users.noreply.github.com>
2017-07-19 21:19:12 -0600

    Enable command line usage for the example script

--- example.py
+++ example.py
@@ -1,13 +1,28 @@
 '''Example script.'''
 
+import argparse
+
 import authorship
 
 
 def main():
     '''Example usage.'''
+    # Build the command line interface
+    parser = argparse.ArgumentParser(
+        description='Determine authorship using a multinomial naive Bayes classifier.')
+    parser.add_argument("classifications",
+        help="Directory containing author writing samples.",
+        type=str,
+        )
+    parser.add_argument("documents",
+        help="Directory containing documents you wish to classify.",
+        type=str,
+        )
+    args = parser.parse_args()
+
     example = authorship.Unigram(stopwords='example/stopwords')
-    classifications = example.get_values('example/foo/classifications')
-    documents = example.get_values('example/foo/documents')
+    classifications = example.get_values(args.classifications)
+    documents = example.get_values(args.documents)
     likelihoods = example.compare_values(classifications, documents)
     example.get_classifications(likelihoods, verbose=True)
 
